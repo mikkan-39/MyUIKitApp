eaf398d26a26dab2d015091b6e317db2edb020f9
Mikkan <egor.altsoft2@gmail.com>
2022-04-22 12:49:31 +0300

    Initial Commit

---
+++ MyUIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUIKitApp
+//
+//  Created by Mikkan Artian on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
