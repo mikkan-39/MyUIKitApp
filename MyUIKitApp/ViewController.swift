5a6493bb161206474108889b4a79feafc3ac16eb
Mikkan <egor.altsoft2@gmail.com>
2022-04-22 12:50:01 +0300

    Init

--- MyUIKitApp/ViewController.swift
+++ MyUIKitApp/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    let data = [ "1", "2", "3" ]
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.frame = view.frame
+        tableView.dataSource = self
+        
+        
+        view.addSubview(tableView)
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = UITableViewCell()
+        let label = UILabel()
+        label.text = data[indexPath.row]
+        label.frame = tableViewCell.frame
+        tableViewCell.addSubview(label)
+        return tableViewCell
+    }
 
 }
 
